b076a0899a88f527b852afa21a404674a433b09c
Sindi_Hall <146654192+Sindihall@users.noreply.github.com>
2024-08-23 22:36:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
+package main
+
+import (
+	"fmt"
+	linkedList "list/package"
+)
+
+func main() {
+	ll := &linkedList.List[int]{Value: 100}
+	ll.Append(110)
+	ll.Append(140)
+	ll.Append(120)
+	ll.Append(109)
+	fmt.Println(ll)
+}
 
